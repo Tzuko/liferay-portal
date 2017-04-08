64ab0318eb32a7c3d8cf4a61587a31f5f466b98a
ndubina <ndubina@emdev.ru>
2017-04-08 21:14:47 +0700

    Ignite hibernate L2 added

---
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/region/LiferayIgniteRegionFactory.java
@@ -0,0 +1,123 @@
+package com.liferay.portal.dao.orm.hibernate.region;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.HashMap;
+
+import org.apache.ignite.internal.IgniteKernal;
+import org.apache.ignite.internal.processors.cache.IgniteInternalCache;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CacheWriteSynchronizationMode;
+import org.apache.ignite.configuration.CacheConfiguration;
+
+import org.hibernate.cache.CacheDataDescription;
+import org.hibernate.cache.CacheException;
+import org.hibernate.cache.CollectionRegion;
+import org.hibernate.cache.EntityRegion;
+import org.hibernate.cfg.Settings;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import ru.emdev.bridge.ignite.cache.hibernate.HibernateRegionFactory;
+import ru.emdev.bridge.ignite.cache.hibernate.HibernateEntityRegion;
+import ru.emdev.bridge.ignite.cache.hibernate.HibernateCollectionRegion;
+
+public class LiferayIgniteRegionFactory extends HibernateRegionFactory {
+
+	private static final Log log = LogFactoryUtil.getLog(LiferayIgniteRegionFactory.class);
+
+	private IgniteKernal ignite;
+	private IgniteInternalCache<Object, Object> cache;
+	private final Map<String, String> regionCaches = new HashMap<>();
+
+	@Override
+	public void start(Settings settings, Properties properties) throws CacheException {
+		super.start(settings, properties);
+		initializeVariables(properties);
+	}
+
+	public void initializeVariables(Properties props) {
+		ignite = (IgniteKernal) Ignition.ignite("liferay-hibernate-grid");
+		String dfltCacheName = props.getProperty(DFLT_CACHE_NAME_PROPERTY);
+
+		for (Map.Entry<Object, Object> prop : props.entrySet()) {
+			String key = prop.getKey().toString();
+			if (key.startsWith(REGION_CACHE_PROPERTY)) {
+				String regionName = key.substring(REGION_CACHE_PROPERTY.length());
+				String cacheName = prop.getValue().toString();
+				regionCaches.put(regionName, cacheName);
+			}
+		}
+		
+		if (dfltCacheName != null) {
+			cache = ((IgniteKernal) ignite).getCache(dfltCacheName);
+		}
+	}
+
+	private IgniteInternalCache<Object, Object> regionCache(String regionName) throws CacheException {
+		String cacheName = regionCaches.get(regionName);
+		if (cacheName == null) {
+			if (cache != null)
+				return cache;
+			cacheName = regionName;
+		}
+		IgniteInternalCache<Object, Object> cache = ((IgniteKernal) ignite).getCache(cacheName);
+		if (cache == null)
+			throw new CacheException("Cache '" + cacheName + "' for region '" + regionName + "' is not configured.");
+		return cache;
+	}
+
+	private CacheConfiguration<Object, Object> cacheConfiguration(String cacheName) {
+		CacheConfiguration<Object, Object> cfg = new CacheConfiguration<Object, Object>();
+		cfg.setName(cacheName);
+		cfg.setCacheMode(CacheMode.PARTITIONED);
+		cfg.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
+		cfg.setWriteSynchronizationMode(CacheWriteSynchronizationMode.FULL_SYNC);
+
+		return cfg;
+	}
+
+	@Override
+	public EntityRegion buildEntityRegion(String regionName, Properties props, CacheDataDescription metadata)
+			throws CacheException {
+		EntityRegion entityRegion = null;
+		try {
+			entityRegion = new HibernateEntityRegion(this, regionName, ignite, regionCache(regionName), metadata);
+		} catch (Exception e) {
+		}
+		if (entityRegion == null) {
+			ignite.createCache(cacheConfiguration(regionName));
+			try {
+				entityRegion = new HibernateEntityRegion(this, regionName, ignite, regionCache(regionName), metadata);
+			} catch (Exception e) {
+				log.debug("exception occurred");
+			}
+		}
+		return entityRegion;
+	}
+
+	@Override
+	public CollectionRegion buildCollectionRegion(String regionName, Properties props, CacheDataDescription metadata)
+			throws CacheException {
+		CollectionRegion collectionRegion = null;
+		try {
+			collectionRegion = new HibernateCollectionRegion(this, regionName, ignite, regionCache(regionName),
+					metadata);
+		} catch (Exception e) {
+		}
+		if (collectionRegion == null) {
+			ignite.createCache(cacheConfiguration(regionName));
+			try {
+				collectionRegion = new HibernateCollectionRegion(this, regionName, ignite, regionCache(regionName),
+						metadata);
+			} catch (Exception e) {
+				log.debug("exception occurred");
+			}
+		}
+		return collectionRegion;
+	}
+
+}
---
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/region/SingletonLiferayIgniteRegionFactory.java
@@ -0,0 +1,95 @@
+package com.liferay.portal.dao.orm.hibernate.region;
+
+import java.util.Properties;
+
+import org.hibernate.cache.CacheDataDescription;
+import org.hibernate.cache.CacheException;
+import org.hibernate.cache.CollectionRegion;
+import org.hibernate.cache.EntityRegion;
+import org.hibernate.cache.QueryResultsRegion;
+import org.hibernate.cache.RegionFactory;
+import org.hibernate.cache.TimestampsRegion;
+import org.hibernate.cache.access.AccessType;
+import org.hibernate.cfg.Settings;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+
+public class SingletonLiferayIgniteRegionFactory implements RegionFactory {
+
+	private static boolean _enabled;
+	private static int _instanceCounter;
+	private static LiferayIgniteRegionFactory _igniteFactory;
+	
+	public LiferayIgniteRegionFactory getInstance(){
+		return _igniteFactory;
+	}
+	
+	public SingletonLiferayIgniteRegionFactory(Properties properties){
+		synchronized (this) {
+			boolean useQueryCache = GetterUtil.getBoolean(properties.get(PropsKeys.HIBERNATE_CACHE_USE_QUERY_CACHE));
+			boolean useSecondLevelCache = GetterUtil
+					.getBoolean(properties.get(PropsKeys.HIBERNATE_CACHE_USE_SECOND_LEVEL_CACHE));
+
+			if (useQueryCache || useSecondLevelCache) {
+				_enabled = true;
+			}
+			
+			if (_igniteFactory == null){
+				_igniteFactory = new LiferayIgniteRegionFactory();
+			}
+		}
+	}
+	
+	@Override
+	public CollectionRegion buildCollectionRegion(String regionName, Properties properties, CacheDataDescription cacheDataDescription)
+			throws CacheException {
+		return _igniteFactory.buildCollectionRegion(regionName, properties, cacheDataDescription);
+	}
+
+	@Override
+	public EntityRegion buildEntityRegion(String regionName, Properties properties, CacheDataDescription cacheDataDescription)
+			throws CacheException {
+		return _igniteFactory.buildEntityRegion(regionName, properties, cacheDataDescription);
+	}
+
+	@Override
+	public QueryResultsRegion buildQueryResultsRegion(String regionName, Properties properties) throws CacheException {
+		return _igniteFactory.buildQueryResultsRegion(regionName, properties);
+	}
+
+	@Override
+	public TimestampsRegion buildTimestampsRegion(String regionName, Properties properties) throws CacheException {
+		return _igniteFactory.buildTimestampsRegion(regionName, properties);
+	}
+
+	@Override
+	public AccessType getDefaultAccessType() {
+		return _igniteFactory.getDefaultAccessType();
+	}
+
+	@Override
+	public boolean isMinimalPutsEnabledByDefault() {
+		return _igniteFactory.isMinimalPutsEnabledByDefault();
+	}
+
+	@Override
+	public long nextTimestamp() {
+		return _igniteFactory.nextTimestamp();
+	}
+
+	@Override
+	public void start(Settings settings, Properties properties) throws CacheException {
+		if (_enabled && (_instanceCounter++ == 0)) {
+			_igniteFactory.start(settings, properties);
+		}
+	}
+
+	@Override
+	public void stop() {
+		if (_enabled && (--_instanceCounter == 0)) {
+			_igniteFactory.stop();
+		}
+	}
+
+}
